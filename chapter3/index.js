19d16bfba87aaece35c88af6ca6c10e4f83b23fb
Nagayoshi <haruna.nagayoshi@asia-quest.jp>
2019-09-29 23:30:02 +0900

    #3章　コンポーネントの基礎　3.2まで
    キーワード：
    グローバルコンポーネント、ローカルコンポーネント、カスタムタグ方式、サブコンストラクタ方式

--- chapter3/index.js
+++ chapter3/index.js
@@ -1,7 +1,39 @@
-Vue.component('list-item', {
-  template: '<li>うぇー</li>'
+Vue.component('description', {
+  template: '<p>季節ごとのくだもの一覧です。</p>',
+});
+
+Vue.component('fruits-table', {
+  template: `
+    <table>
+        <tr>
+            <th>季節</th>
+            <th>くだもの</th>
+        </tr>    
+        <tr>
+            <td>春</td>
+            <td>いちご</td>
+        </tr>   
+        <tr>
+            <td>夏</td>
+            <td>ぶどう</td>
+        </tr>  
+        <tr>
+            <td>秋</td>
+            <td>なし</td>
+        </tr>  
+        <tr>
+            <td>冬</td>
+            <td>みかん</td>
+        </tr>  
+    </table>
+  `,
 });
 
 new Vue({
-  el: '#example',
+  el: '#fruits-list',
+  components: {
+    'fruits-list-title': {
+      template: '<h1>くだものリスト</h1>',
+    },
+  },
 });
