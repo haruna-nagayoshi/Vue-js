d8cd1b11ba02396469dd44b7b16a1a3377ba0ab6
AQ_haruna_nagayoshi <haruna.nagayoshi@asia-quest.jp>
2019-10-01 18:37:40 +0900

    #6章　単一ファイルコンポーネントによる開発　完
    ・単一ファイルコンポーネントの動作、ビルド方法などを確認した。
    ・dockerコマンドの説明および参考文献をREADMEに追加した。
    
    キーワード:
    .vue拡張子、単一ファイルコンポーネント、Vue CLI、スコープ付きCSS

---
+++ src/chapter6/test/hello.vue
@@ -0,0 +1,19 @@
+<template>
+    <p class="message">ひとこと: {{ msg }}</p>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        msg: 'へろー'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+    .message {
+        color: #42b983;
+    }
+</style>
